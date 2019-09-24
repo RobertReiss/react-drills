abbb665981530945b720dc93a55b9f22b2c71f88
Robert Reiss <reissaudiodesign@yahoo.com>
2019-09-24 15:02:46 -0600

    completed problem

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+    filterString: '',
+    books: [`The Wise Man's Fear`, 'Darth Maul: Lockdown', 'The Historical Jesus', `Cursor's Fury`]
+    }
+  }
+
+  handleChange(filter){
+    this.setState({filterString: filter})
+  }
+
+  
+
   render() {
+
+    let booksToDisplay = this.state.books.filter((element, index) => {
+      return element.includes(this.state.filterString)
+    })
+    .map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+    
+    
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    <div className="App">
+      <input onChange={e => this.handleChange(e.target.value)} type='text' /> 
+    {booksToDisplay}
+    </div>
+    )
   }
 }
 
